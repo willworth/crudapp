127483a98894a3d6741c8b7781ce5ad9be67ebfc
Will Worth <will.worth@xyz.com>
2020-09-13 14:27:45 +0200

    ss

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const chalk = require('chalk'); // https://www.npmjs.com/package/chalk
 const app = express();
 const PORT = 7777
 
 app.get('/', (req, res) => {
     res.send('sending from default route');
 });
-
 app.listen(PORT, () => {
-    console.log(`running on port ${PORT}`);
+    console.log(chalk.black.bgGreen.bold(`running on port ${PORT}`));
+
 })
